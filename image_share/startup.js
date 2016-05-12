79d602056d2f8bcc5185c4bc01e0f493550201f0
Sneghka <sneghkan@mail.ru>
2016-05-12 17:59:18 +0300

    4-th lesson

--- image_share/startup.js
+++ image_share/startup.js
@@ -3,15 +3,13 @@ if (Meteor.isServer) {
         if (Images.find().count() == 0) {
 
             for (var i = 1; i < 23; i++) {
-                Images.insert(
-                    {
-                    img_src: "img_" + i +".jpg",
+                Images.insert({
+                    img_src: "img_" + i + ".jpg",
                     img_alt: "image number" + i
                 });
             }
-            console.log(Images.find().count());
-        } // end of if
+                console.log("startup.js says: " + Images.find().count());
 
-
+        } // end of if have images
     });
 }
